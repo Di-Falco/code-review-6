d680104e3d7e5380c07afa8bbcffa9f74cc2d4a3
Anthony DiFalco <aodifalco@gmail.com>
2022-07-01 15:52:01 -0700

    refactor code

--- src/index.js
+++ src/index.js
@@ -4,10 +4,57 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Bank from './bank.js';
 
+// empty all necessary fields
+function clearFields() {
+  $("#error").text("");
+  $("#amountTo").text("");
+}
+
+// check to see if a valid currency has been selected
+async function checkResponse(validCurrency, currency) {
+  if (!validCurrency.result) {
+    $("#error").text(`Error: ${currency} is not a supported currency`);
+    return false;
+  }
+  return true;
+}
+
+// display the results of converting between selected currencies
+function displayConversion(conversion, currencies, symbols) {
+  if (conversion.result) {
+    $("#amountTo").text(`${symbols[currencies.indexOf(conversion.base_code)]} ${(conversion.conversion_result).toFixed(2)}`);
+  } else {
+    $("#error").text(`Error: ${conversion}`);
+  }
+}
+
+// display exchange rate for 1 unit of input currency in each currency
+async function displayRates(currencyFrom, currencies, symbols) {
+  $("#mainCurrencyFrom").text(`Value of 1 ${currencyFrom} in:`);
+  $("#mainCurrencyFrom").append(`<br><br>`);
+  for (let i=0; i<currencies.length; i++) {
+    let oneConversion = await Bank.getExchangeRate(currencyFrom, currencies[i], 1);
+    $("#mainCurrencyFrom").append(`${currencies[i]}: ${symbols[i]}${(oneConversion.conversion_result).toFixed(2)}<br>`);
+  }
+}
+
+// make api call for 1:1 exchange
+async function getCurrencyApiCall(currency) {
+  const response = await Bank.getCurrencies(currency);
+  return response;
+}
+
+// make api call for each exchange rate
+async function getExchangeApiCall(currencyFrom, currencyTo, amount) {
+  const response = await Bank.getExchangeRate(currencyFrom, currencyTo, amount);
+  return response;
+}
+
+// UI
 $(document).ready(function(){
 
-  $("#exchange").onclick(async function(){
-    $("#error").text("");
+  $("#exchange").click(async function(){
+    clearFields();
     const currencies = ["USD", "GBP", "EUR", "JPY", "KRW", "NPR"];
     const symbols = ["$", "£", "€", "￥", "₩", "₨"];
 
@@ -15,35 +62,20 @@ $(document).ready(function(){
     let currencyTo = $("#currencyTo").val();
     let amount = parseInt($("#amountFrom").val());
 
-    let validCurrenciesFrom = await Bank.getCurrencies(`${currencyFrom}`);
-    let validCurrenciesTo = await Bank.getCurrencies(`${currencyTo}`);
+    let validCurrenciesFrom = await getCurrencyApiCall(currencyFrom);
+    let validCurrenciesTo = await getCurrencyApiCall(currencyTo);
 
     if (!Number(amount)) {
       $("#error").text(`Error: Please input a number value to exchange`);
-    } else if (!validCurrenciesFrom.result) {
-      $("#error").text(`Error: ${currencyFrom} is not a supported currency`);
+    } else if (!checkResponse(validCurrenciesFrom, currencyFrom)) {
       return 0;
-    } else if (!validCurrenciesTo.result) {
-      $("#error").text(`Error: ${currencyTo} is not a supported currency`);
+    } else if (!checkResponse(validCurrenciesTo, currencyTo)) {
       return 0;
     } else {
-
-      let conversion = await Bank.getExchangeRate(currencyFrom, currencyTo, amount);
-      if (conversion.result) {
-        $("#amountTo").text(`${symbols[currencies.indexOf(currencyTo)]} ${(conversion.conversion_result).toFixed(2)}`);
-      } else {
-        $("#error").text(`Error: ${conversion}`);
-      }
-
-      // print conversion from the input amount for each currency
-      $("#mainCurrencyFrom").text(`Value of 1 ${currencyFrom} in:`);
-      $("#mainCurrencyFrom").append(`<br><br>`);
-      for (let i=0; i<currencies.length; i++) {
-        let oneConversion = await Bank.getExchangeRate(currencyFrom, currencies[i], 1);
-        $("#mainCurrencyFrom").append(`${currencies[i]}: ${symbols[i]}${(oneConversion.conversion_result).toFixed(2)}<br>`);
-      }
-
+      let conversion = await getExchangeApiCall(currencyFrom, currencyTo, amount);
+      
+      displayConversion(conversion, currencies, symbols);
+      displayRates(currencyFrom, currencies, symbols);
     }
   });
-
 });
