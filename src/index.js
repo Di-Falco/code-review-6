4968ac7fddef814f76d9c5ae80c7a2db222a0df5
Anthony DiFalco <aodifalco@gmail.com>
2022-07-01 09:50:06 -0700

    add exchange options for GBP and EUR

--- src/index.js
+++ src/index.js
@@ -7,11 +7,18 @@ import Bank from './bank.js';
 $(document).ready(function(){
 
   $("#exchange").click(async function(){
+    const currencies = ["USD", "GBP", "EUR", "JPY"];
+    const symbols = ["$", "£", "€", "￥"];
+
     let currencyFrom = $("#currencyFrom").val();
     let currencyTo = $("#currencyTo").val();
     let amount = parseInt($("#amountFrom").val());
 
-    console.log(await Bank.getExchangeRate(currencyFrom, currencyTo, amount));
+    let conversion = await Bank.getExchangeRate(currencyFrom, currencyTo, amount);
+
+    $("#amountTo").text(`${symbols[currencies.indexOf(currencyTo)]} ${(conversion.conversion_result).toFixed(2)}`);
+
+    console.log(conversion);
   });
 
 });
